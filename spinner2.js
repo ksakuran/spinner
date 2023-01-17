0ba8faae34fa2320ddfb19196f439ca56660636d
Kat Nishimura <katherine.nishimura@live.ca>
2023-01-17 22:47:21 +0000

    a refactored version of spinner1.js, putting the characters in an array and looping with a settimeout and incrementing delay

---
+++ spinner2.js
@@ -0,0 +1,25 @@
+let spinChar = ["|", "/", "-", "\\","|", "/", "-", "\\","|", "/", "-", "\\", "|", "/", "-", "\\"];
+let delay = 100;
+for(const char of spinChar) {
+  setTimeout(() => {
+    process.stdout.write(`\r${char}   `);
+  }, delay += 200)
+};
+
+
+
+// setTimeout(() => {
+//   process.stdout.write('\r|  ');
+// }, 100);
+
+// setTimeout(() => {
+//   process.stdout.write('\r/  ');
+// }, 300);
+
+// setTimeout(() => {
+//   process.stdout.write('\r-  ');
+// }, 500);
+
+// setTimeout(() => {
+//   process.stdout.write('\r\\  ');
+// }, 700);
